a9f405865a4012a6a20d5f415fea0a940f7c2112
meenujangra-sk <meenu.j@sketchbrains.com>
2020-09-07 12:37:18 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+      },
+    },
+    
+    extend: {
+      colors: {
+        text_dark: '#343a40',
+        color_primary: '#007bff',
+        color_secondry: '#1D2B4C',
+        color_gry: '#6c757d',
+        color_blck: '#212529',
+        button_orange: '#fa6400',
+      },    
+    },
+    
+  },
+  variants: {},
+  plugins: [],
+}
